84cc58c731714806673e5a608099dc1ccebb99ab
Umiterr <37944964+Umiterr@users.noreply.github.com>
2023-08-18 19:28:04 -0600

    add validator.js/fix function & responsive issues

--- scripts/index.js
+++ scripts/index.js
@@ -138,7 +138,7 @@ function addNewPost(event) {
 
   formPostName.value = "";
   formPostURL.value = "";
-  DisablePostButton();
+  disablePostButton();
 }
 
 postButtonSave.addEventListener("click", addNewPost);
@@ -214,76 +214,3 @@ initialCards.forEach(function (item, i) {
   savePost(item.link, item.name);
 });
 
-// Form validator
-
-const showInputError = (formElement, inputElement, errorMessage) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add("form__input_type_error");
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add("form__input-error_active");
-};
-
-const hideInputError = (formElement, inputElement) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove("form__input_type_error");
-  errorElement.classList.remove("form__input-error_active");
-  errorElement.textContent = "";
-};
-
-const checkInputValidity = (formElement, inputElement) => {
-  if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
-  } else {
-    hideInputError(formElement, inputElement);
-  }
-};
-
-const setEventListeners = (formElement) => {
-  const inputList = Array.from(formElement.querySelectorAll(".form__input"));
-  const buttonElement = formElement.querySelector(".form__submit");
-  inputList.forEach((inputElement) => {
-    inputElement.addEventListener("input", function () {
-      checkInputValidity(formElement, inputElement);
-      toggleButtonState(inputList, buttonElement);
-    });
-  });
-};
-
-const enableValidation = () => {
-  const formList = Array.from(document.querySelectorAll(".form"));
-  formList.forEach((formElement) => {
-    formElement.addEventListener("submit", function (evt) {
-      evt.preventDefault();
-    });
-
-    setEventListeners(formElement);
-  });
-};
-
-enableValidation();
-
-// Button State
-
-const hasInvalidInput = (inputList) => {
-  return inputList.some((inputElement) => {
-    return !inputElement.validity.valid;
-  });
-};
-
-const toggleButtonState = (inputList, buttonElement) => {
-  if (hasInvalidInput(inputList)) {
-    buttonElement.classList.add("form__submit_inactive");
-    buttonElement.disabled = true;
-  } else {
-    buttonElement.classList.remove("form__submit_inactive");
-    buttonElement.disabled = false;
-  }
-};
-
-//Disables Post button after adding a new post
-const DisablePostButton = () => {
-  const formPostElement = document.querySelector(".form-post");
-  const buttonPostElement = formPostElement.querySelector(".form-post__save");
-  buttonPostElement.classList.add("form__submit_inactive");
-  buttonPostElement.disabled = true;
-};
